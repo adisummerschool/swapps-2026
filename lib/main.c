4c0c15b42e5b54cfbe1006f83fbe92d97020fa6b
Emilia Pedolu <emiliapedolu@gmail.com>
2026-08-17 11:47:00 +0300

    Added the Makefile, libfunc.c, libfunc.h, main.c

---
+++ lib/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <iio.h>
+#include "libfunc.h"
+
+int main () {
+    printf("hello, world!\n");
+    //printf("%d\n", iio_get_backends_count());
+    func();
+    return 0;
+}
