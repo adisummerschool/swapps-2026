a7facb921e35c6a451b3fe4db2b15ad69da79a5f
Doaga Petrut-Stefan <doaga.petrut.stefan@gmail.com>
2026-08-17 10:18:16 +0300

    workspace/swapps/swapps-2026: Read channel
    Create C program to read AD5592R channels.
    
    Signed-off-by: Doaga Petrut-Stefan <doaga.petrut.stefan@gmail.com>

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <iio.h>
+
+int main() {
+        printf("hello world\n");
+        printf("%d\n", iio_get_backends_count());
+
+        struct iio_context *cont = iio_create_context_from_uri("ip:10.76.84.7");
+        struct iio_device *dev = iio_context_find_device(cont, "iio_ad5592r_s");
+        struct iio_channel *chan0 = iio_device_get_channel(dev, 0);
+        const char *attr = iio_channel_get_attr(chan0, 0);
+        long long val = 0;
+        iio_channel_attr_read_longlong(chan0, attr, &val);
+        printf("%lld\n", val);
+
+        return 0;
+}
