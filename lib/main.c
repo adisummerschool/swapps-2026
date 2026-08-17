059ff6c52d56c41d7f947d2ed6af2c46b462c600
Dradici Leon <dradici.leon5@gmail.com>
2026-08-17 11:37:31 +0300

    Added gcc and makefile apps
    Signed-off-by: Dradici Leon <dradici.leon5@gmail.com>

---
+++ lib/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "libfunc.h"
+
+
+
+int main () {
+        printf("hello, world!\n");
+        //printf("%d\n", iio_get_backends_count());
+        
+        func();
+
+        return 0;
+}
