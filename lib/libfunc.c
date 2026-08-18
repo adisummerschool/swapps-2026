3359b7f0522d6b42563c6d70489c37624eba8aae
teodormorosanu <teodor.ioan.morosanu@gmail.com>
2026-08-18 11:53:21 +0300

    Added shock register function
    Detects is the board feeled more than 1.5G
    
    Signed-off-by: teodormorosanu <teodor.ioan.morosanu@gmail.com>

--- lib/libfunc.c
+++ lib/libfunc.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <iio.h>
 #include <unistd.h>
+#include <math.h>
 
 long long func(int channel) {
     struct iio_context* cont = iio_create_context_from_uri("ip:10.76.84.31");
@@ -117,11 +118,37 @@ void buffer() {
     }
 
     void *start = iio_buffer_start(buf);
-    long long int val;
+    long long int val[6] = {0, 0, 0, 0, 0, 0};
     int i = 0;
-    for (void* ptr = start; ptr < iio_buffer_end(buf); ptr += iio_buffer_step(buf)) {
-        iio_channel_convert(chan[0], &val, ptr);
-        printf("%lld\n", val);
+    double alpha = 0.9, g_x = 0, g_y = 0, g_z = 0;
+    while (true) {
+        for (void* ptr = start; ptr < iio_buffer_end(buf); ptr += iio_buffer_step(buf)) {
+            iio_channel_convert(chan[0], &val[0], ptr);
+            iio_channel_convert(chan[1], &val[1], ptr + 2);
+            iio_channel_convert(chan[2], &val[2], ptr + 4);
+            iio_channel_convert(chan[3], &val[3], ptr + 6);
+            iio_channel_convert(chan[4], &val[4], ptr + 8);
+            iio_channel_convert(chan[5], &val[5], ptr + 10);
+
+            int ax = val[0] - val[1];
+            int ay = val[2] - val[3];
+            int az = val[4] - val[5];
+
+            g_x = alpha * g_x + (1.0 - alpha) * (double)ax;
+            g_y = alpha * g_y + (1.0 - alpha) * (double)ay;
+            g_z = alpha * g_z + (1.0 - alpha) * (double)az;
+
+            double shock_x = (double)ax - g_x;
+            double shock_y = (double)ay - g_y;
+            double shock_z = (double)az - g_z;
+
+            double shock = sqrt(shock_x * shock_x + shock_y * shock_y + shock_z * shock_z);
+            if (shock > 3072) {
+                printf("Detected shock %.2fG\n", shock / 2048);
+            }
+
+        }
+        int ret = iio_buffer_refill(buf);
     }
     iio_buffer_destroy(buf);
 }
