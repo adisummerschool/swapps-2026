e0f31a48dfa11fef34b24c6fba655d54172d1efe
Maya Mascasan <mascasan.se.maya@student.utcluj.ro>
2026-08-18 17:03:53 +0300

    swapps-2026: python_gui: Added IIO motion data acquisition flow
    - Added initial IIO device setup for the motion sensor.
    - Implemented get_data() to read x, y, and z axis values from the device channels.
    - Added startup logic to initialize the device and process the captured sensor data.
    - Wired the threaded data flow into the queue-based application structure.
    
    Signed-off-by: Maya Mascasan <mascasan.se.maya@student.utcluj.ro>

---
+++ main.py
@@ -0,0 +1,67 @@
+import iio
+from queue import Queue
+from threading import Thread
+
+def create_movement_gui(queue: Queue):
+    pass
+
+def get_data(device):
+    axis_data = {
+        'x':{'-': int, '+': int},
+        'y':{'-': int, '+': int},
+        'z':{'-': int, '+': int},  
+    }
+    #populate axis_data from iio
+    axes = ['x','y','z']
+    channel_names = [f'voltage{i}' for i in range(6)]
+    for i,channel_name in enumerate(channel_names):
+        ch = device.find_channel(channel_name)
+        if ch is None:
+            raise ValueError("could not find channel")
+
+        attr = ch.attrs['raw'].value
+        
+        axis = axes[i // 2]
+        polarity = '+' if i%2 == 0 else '-'
+        axis_data[axis][polarity] = attr
+
+    return axis_data
+
+def start_iio(device):
+    data = get_data(device)
+    print(data)
+    #roll, pitch = get_roll_pitch(data)
+    #movement = get_movement(roll,pitch)
+
+    #return movement
+
+def threaded_function(queue: Queue):
+    device = init_iio()
+    #while True:
+    queue.put(start_iio(device))
+
+def init_iio():
+    ctx = iio.Context("ip:10.76.84.15")
+    device = ctx.find_device("ad5592r_s")
+    if device is None:
+        raise ValueError("Failed to get device")
+    return device
+
+if __name__ == '__main__':
+
+    #pause_event = Event()
+    #start_event = Event()
+
+    movement = {'left': 0, 'right': 0, 'front': 0, 'back': 0}
+    movement_queue = Queue()
+    movement_queue.put(movement)
+
+    thread = Thread(target=create_movement_gui, args=(movement_queue,))
+    thread.daemon = True
+    thread.start()
+
+    thread = Thread(target=threaded_function, args=(movement_queue,))
+    thread.daemon = True
+    thread.start()
+
+    thread.join()
