adfe712b81ed732e3fd3ab88b4649dc7b23ca176
Denisa Dersedan <denisa.dersedan@analog.com>
2026-08-19 15:53:27 +0300

    python: Add GUI and simulation of WASD keys
    - Add accelerometer-based WASD controls with movement GUI
    - Map accelerometer tilt to keyboard input
    
    Signed-off-by: Denisa Dersedan <denisa.dersedan@analog.com>

--- python/key_sim.py
+++ python/key_sim.py
@@ -3,10 +3,46 @@ from queue import Queue
 from threading import Thread
 from threading import Event
 from pynput import keyboard
+from pynput.keyboard import Controller
 import math
+import tkinter as tk
+import time
+
+TIME = 0.1
 
 def create_movement_gui(queue: Queue):
-    pass
+    def update_square(canvas, square, on_time):
+        intensity = 255 - int(on_time * 255)
+        color = f'#{intensity:02x}{intensity:02x}ff'
+        canvas.itemconfig(square, fill=color)
+
+    def update_gui():
+        mov = queue.get()
+
+        update_square(canvas, squares['left'], mov['left'])
+        update_square(canvas, squares['right'], mov['right'])
+        update_square(canvas, squares['front'], mov['front'])
+        update_square(canvas, squares['back'], mov['back'])
+
+        root.after(int(TIME * 1000), update_gui)
+    
+    root = tk.Tk()
+    canvas = tk.Canvas(root, width=400, height=400);
+    canvas.pack()
+
+    square_size = 100
+    squares = {
+        'left' : canvas.create_rectangle(50, 150, 50 + square_size, 150 + square_size, fill='white'),
+        'right' : canvas.create_rectangle(50 + 2 * square_size, 150, 50 + 3 * square_size, 150 + square_size, fill='white'),
+        'front' : canvas.create_rectangle(150, 50, 150 + square_size, 50 + square_size, fill='white'),
+        'back' : canvas.create_rectangle(150, 150, 150 + square_size, 150 + square_size, fill='white'),
+    }
+
+    update_gui()
+
+    root.mainloop()
+
+    mov = queue.get()
 
 def get_data(device):
     axis_data = {
@@ -57,18 +93,34 @@ def get_movement(roll, pitch):
         mov['left'] = min(1.0, -roll)
 
     return mov
-    
+
+def threaded_keypress(key, on_time):
+    kb_controller = Controller()
+
+    kb_controller.press(key)
+    time.sleep(on_time)
+    kb_controller.release(key)
 
 def start_iio(device):
+    start = time.time()
     data = get_data(device)
-    #print(data)
 
     roll, pitch = get_roll_pitch(data)
     movement = get_movement(roll, pitch)
 
-    print(movement)
+    iio_timer = time.time() - start
 
-    #return movement
+    dir2key = {'left' : 'a', 'right' : 'd', 'front' : 'w', 'back' : 's'}
+
+    for dir,val in movement.items():
+        if val > 0:
+            thread = Thread(target=threaded_keypress, args=(dir2key[dir], (TIME * val)))
+            thread.daemon = True
+            thread.start()
+
+    time.sleep(abs(TIME - iio_timer))
+
+    return movement
 
 def threaded_function(queue: Queue):
     device = init_iio()
@@ -104,7 +156,6 @@ def on_keypress(key):
 if __name__ == '__main__':
 
     pause_event = Event()
-    pause_event.set()
     stop_event = Event()
 
     movement = {'left': 0, 'right': 0, 'front': 0, 'back': 0}
