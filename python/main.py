fc8991f6e67f47da041728b2ea5d2c3438dbb498
Denisa Stanut <stanutdenisa099@gmail.com>
2026-08-19 15:52:14 +0300

    /python: Added movement control - main.py: - added GUI for displaying movement directions 	   - added reading of x, y and z axis values from ADC channels 	   - added roll and pitch 	   - added keyboard control using W, A, S and D keys 	   - added threads for GUI, IIO reading and keypress handling
    Signed-off-by: Denisa Stanut <stanutdenisa099@gmail.com>
    
    Signed-off-by: Denisa Stanut <stanutdenisa099@gmail.com>

--- python/main.py
+++ python/main.py
@@ -1,9 +1,47 @@
 import iio
 from queue import Queue
 from threading import Thread, Event
+from pynput import keyboard
+from pynput.keyboard import Controller
+import math
+import tkinter as tk
+import time
 
-def create_movement_qui(queue: Queue):
-    pass
+TIME = 0.1
+
+
+def create_movement_gui(queue: Queue):
+    def update_square(canvas, square, on_time):
+        intensity = 255 - int(on_time * 255)
+        color = f'#{intensity:02x}{intensity:02x}ff'
+        canvas.itemconfig(square, fill=color)
+
+    def update_gui():
+        mov = queue.get()
+        update_square(canvas, squares['left'], mov['left'])
+        update_square(canvas, squares['right'], mov['right'])
+        update_square(canvas, squares['front'], mov['front'])
+        update_square(canvas, squares['back'], mov['back'])
+
+        root.after(int(TIME * 1000), update_gui)
+
+    root = tk.Tk()
+    canvas = tk.Canvas(root, width=400,  height=400)
+    canvas.pack()
+
+    square_size = 100
+    squares = {
+        'left': canvas.create_rectangle(50, 150, 50 + square_size, 150 + square_size, fill='white'),
+        'right': canvas.create_rectangle(50 + 2 * square_size, 150, 50 + 3 * square_size, 150 + square_size, fill='white'),
+        'front': canvas.create_rectangle(150, 50, 150 + square_size, 50 + square_size, fill='white'),
+        'back': canvas.create_rectangle(150, 150, 150 + square_size, 150 + square_size, fill='white')
+    }
+
+    update_gui()
+    root.mainloop()
+
+    mov = queue.get()
+
 
 def get_data(device):
     axis_data = {
@@ -24,30 +62,88 @@ def get_data(device):
 
         axis = axes[i // 2]
         polarity = '+' if i % 2 == 0 else '-'
-        axis_data[axis][polarity] = attr
+        axis_data[axis][polarity] = int(attr)
 
     return axis_data
 
-def get_roll_pitch(data):
-    pass
+
+def get_roll_pitch(axis: dict):
+    x = axis['x']['+'] - axis['x']['-']
+    y = axis['y']['+'] - axis['y']['-']
+    z = axis['z']['+'] - axis['z']['-']
+
+    roll = math.atan2(y, z) * 100 / math.pi
+    pitch = math.atan2(-x, math.sqrt(y ** 2 + z ** 2)) * 100 / math.pi
+
+    return roll, pitch
+
 
 def get_movement(roll, pitch):
-    pass
+    mov = {
+        'left': 0,
+        'right': 0,
+        'back': 0,
+        'front': 0
+    }
+
+    roll /= 45
+    pitch /= 45
+
+    if roll > 0.2:
+        mov["right"] = min(1, roll)
+    elif roll < -0.2:
+        mov["left"] = min(1, -roll)
+
+    if pitch > 0.2:
+        mov["front"] = min(1, pitch)
+    elif pitch < -0.2:
+        mov["back"] = min(1, -pitch)
+
+    return mov
+
+
+def threaded_keypress(key, on_time):
+    kb_controller = Controller()
+
+    kb_controller.press(key)
+    time.sleep(on_time)
+    kb_controller.release(key)
+
 
 def start_iio(device):
+    start = time.time()
     data = get_data(device)
-    print(data)
+    roll, pitch = get_roll_pitch(data)
+    movement = get_movement(roll, pitch)
+    iio_timer = time.time() - start
 
-    # roll, pitch = get_roll_pitch(data)
-    # movement = get_movement(roll, pitch)
+    keys = {
+        'left': 'a',
+        'right': 'd',
+        'front': 'w',
+        'back': 's'
+    }
 
-    # return movement
+    # iterate through each direction and spawn a keypress thread
+    for dir, value in movement.items():
+        if value > 0:
+            thread = Thread(target=threaded_keypress, args=(keys[dir], TIME * value))
+            thread.daemon = True
+            thread.start()
+
+    time.sleep(abs(TIME - iio_timer))
+
+    return movement
+
 
 def threaded_function(queue: Queue):
     device = init_iio()
 
-    #while True:
-    queue.put(start_iio(device))
+    while not stop_event.is_set():
+        if pause_event.is_set():
+            queue.put(start_iio(device))
+        pause_event.wait()
+
 
 def init_iio():
     context = iio.Context("ip:10.76.84.24")
@@ -59,22 +155,45 @@ def init_iio():
     return device
 
 
+def on_keypress(key):
+    if key == keyboard.Key.ctrl_l:
+        if pause_event.is_set():
+            pause_event.clear()
+            print("Paused")
+        else:
+            pause_event.set()
+            print("Resumed")
+    elif key == keyboard.Key.esc:
+        pause_event.set()
+        stop_event.set()
+        print("Exit")
+        return False
+
+    return True
+
+
 if __name__ == '__main__':
+    pause_event = Event()
+    stop_event = Event()
+
     movement = {
-        'left': 0, 
-        'right': 0, 
-        'back': 0, 
+        'left': 0,
+        'right': 0,
+        'back': 0,
         'front': 0
     }
     movement_queue = Queue()
     movement_queue.put(movement)
 
-    thread = Thread(target=create_movement_qui, args=(movement_queue, ))
+    thread = Thread(target=create_movement_gui, args=(movement_queue, ))
     thread.daemon = True
     thread.start()
 
     thread = Thread(target=threaded_function, args=(movement_queue, ))
     thread.daemon = True
     thread.start()
+
+    with keyboard.Listener(on_press=on_keypress) as listener:
+        listener.join()
 
     thread.join()
